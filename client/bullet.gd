2bddb5429841b73f00591ee48b8049a666d70cf9
MystixCode <mystixrage@gmx.ch>
2022-04-10 03:12:14 +0200

    bullet synced_position

--- client/bullet.gd
+++ client/bullet.gd
@@ -1,3 +1,10 @@
 extends RigidDynamicBody3D
 
 var from_player : int
+var synced_position : Vector3
+
+func _physics_process(delta):
+	
+	if synced_position:
+		#print(synced_position)
+		position=synced_position
